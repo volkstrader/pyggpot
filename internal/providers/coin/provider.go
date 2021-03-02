e467bb785114fb563ed7c1259309b1ef4e67d6c4
Chuck Chau <volkstrader@users.noreply.github.com>
2021-03-02 15:47:53 -0800

    Task2 (#2)
    * Code the RemoveCoins service
    
    * update and report on the Kind of coin that has been removed
    
    * don not rollback if committed

--- internal/providers/coin/provider.go
+++ internal/providers/coin/provider.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"math/rand"
+
 	"github.com/aspiration-labs/pyggpot/internal/models"
-	"github.com/aspiration-labs/pyggpot/rpc/go/coin"
+	coin_service "github.com/aspiration-labs/pyggpot/rpc/go/coin"
 	"github.com/twitchtv/twirp"
 )
 
-type coinServer struct{
+type coinServer struct {
 	DB *sql.DB
 }
 
@@ -31,9 +33,9 @@ func (s *coinServer) AddCoins(ctx context.Context, request *coin_service.AddCoin
 	for _, coin := range request.Coins {
 		fmt.Println(coin)
 		newCoin := models.Coin{
-			PotID: request.PotId,
+			PotID:        request.PotId,
 			Denomination: int32(coin.Kind),
-			CoinCount: coin.Count,
+			CoinCount:    coin.Count,
 		}
 		err = newCoin.Save(tx)
 		if err != nil {
@@ -50,6 +52,95 @@ func (s *coinServer) AddCoins(ctx context.Context, request *coin_service.AddCoin
 	}, nil
 }
 
-func (s *coinServer) RemoveCoins(context.Context, *coin_service.RemoveCoinsRequest) (*coin_service.CoinsListResponse, error) {
-	panic("implement me")
+func (s *coinServer) RemoveCoins(ctx context.Context, request *coin_service.RemoveCoinsRequest) (*coin_service.CoinsListResponse, error) {
+	if err := request.Validate(); err != nil {
+		return nil, twirp.InvalidArgumentError(err.Error(), "")
+	}
+
+	tx, err := s.DB.Begin()
+	if err != nil {
+		return nil, twirp.InternalError(err.Error())
+	}
+
+	// CoinsInPotsByPot_id
+	pot, err := models.CoinsInPotsByPot_id(tx, int(request.PotId))
+	if err != nil {
+		return nil, twirp.InternalError(err.Error())
+	}
+
+	coins := make([]*models.Coin, len(pot))
+	for i, c := range pot {
+		coins[i], err = models.CoinByID(tx, c.ID)
+		if err != nil {
+			return nil, twirp.InternalError(err.Error())
+		}
+	}
+
+	// shake pot
+	final := shakePot(pot, int(request.Count), rand.Int)
+	committed := false
+	defer func() {
+		if !committed {
+			_ = tx.Rollback()
+		}
+	}()
+
+	resp := coin_service.CoinsListResponse{
+		Coins: make([]*coin_service.Coins, len(pot)),
+	}
+
+	for i, c := range coins {
+		if c.CoinCount == int32(final[i]) {
+			// skip update when no changes
+			continue
+		}
+
+		c.CoinCount = int32(final[i])
+		if err = c.Update(tx); err != nil {
+			return nil, twirp.InternalError(err.Error())
+		}
+
+		resp.Coins[i] = &coin_service.Coins{
+			Kind:  coin_service.Coins_Kind(c.Denomination),
+			Count: int32(final[i]),
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return nil, twirp.NotFoundError(err.Error())
+	}
+	committed = true
+	return &resp, nil
+}
+
+// shakePot simulate a person pickup the pyggot (lock the resource exclusively) and then shake the pot to remove coin
+// the person will let go the pot (unlock the resource) after getting the count or all coins out of the pot
+func shakePot(pot []*models.CoinsInPot, count int, randInt func() int) []int {
+	total := 0
+	tally := make([]int, len(pot))
+	for i, c := range pot {
+		x := int(c.CoinCount)
+		tally[i] = x
+		total += x
+	}
+
+	if total == 0 || count >= total {
+		return make([]int, len(pot))
+	}
+
+	for i := count; i > 0; i-- {
+		n := randInt()%total + 1
+		total--
+		m := 0
+		for j, c := range tally {
+			m += c
+			if n <= m {
+				tally[j]--
+				break
+			}
+		}
+	}
+
+	return tally
 }
--- internal/providers/coin/provider_test.go
+++ internal/providers/coin/provider_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/aspiration-labs/pyggpot/internal/models"
+
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 	"github.com/stretchr/testify/assert"
 
@@ -69,5 +71,146 @@ func TestFixAddCoinValidationMessage(t *testing.T) {
 			assert.NoError(t, err)
 		})
 	}
+}
 
+func TestShakePot(t *testing.T) {
+	type testCase struct {
+		name  string
+		pot   []*models.CoinsInPot
+		draws []int
+		want  []int
+	}
+
+	tests := []testCase{
+		{
+			name:  "zero pot",
+			pot:   []*models.CoinsInPot{},
+			draws: []int{1, 2, 3},
+			want:  []int{},
+		},
+		{
+			name: "empty pot",
+			pot: []*models.CoinsInPot{
+				{
+					ID:           0,
+					Denomination: 1,
+					CoinCount:    0,
+				},
+				{
+					ID:           1,
+					Denomination: 5,
+					CoinCount:    0,
+				},
+				{
+					ID:           2,
+					Denomination: 10,
+					CoinCount:    0,
+				},
+			},
+			draws: []int{1, 2, 3},
+			want:  []int{0, 0, 0},
+		},
+		{
+			name: "draw more",
+			pot: []*models.CoinsInPot{
+				{
+					ID:           0,
+					Denomination: 1,
+					CoinCount:    10,
+				},
+				{
+					ID:           1,
+					Denomination: 5,
+					CoinCount:    5,
+				},
+				{
+					ID:           2,
+					Denomination: 10,
+					CoinCount:    1,
+				},
+			},
+			draws: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18},
+			want:  []int{0, 0, 0},
+		},
+		{
+			name: "draw front",
+			pot: []*models.CoinsInPot{
+				{
+					ID:           0,
+					Denomination: 1,
+					CoinCount:    3,
+				},
+				{
+					ID:           1,
+					Denomination: 5,
+					CoinCount:    2,
+				},
+				{
+					ID:           2,
+					Denomination: 10,
+					CoinCount:    2,
+				},
+			},
+			draws: []int{2, 1, 0},
+			want:  []int{0, 2, 2},
+		},
+		{
+			name: "draw middle",
+			pot: []*models.CoinsInPot{
+				{
+					ID:           0,
+					Denomination: 1,
+					CoinCount:    3,
+				},
+				{
+					ID:           1,
+					Denomination: 5,
+					CoinCount:    2,
+				},
+				{
+					ID:           2,
+					Denomination: 10,
+					CoinCount:    2,
+				},
+			},
+			draws: []int{3, 3},
+			want:  []int{3, 0, 2},
+		},
+		{
+			name: "draw end",
+			pot: []*models.CoinsInPot{
+				{
+					ID:           0,
+					Denomination: 1,
+					CoinCount:    3,
+				},
+				{
+					ID:           1,
+					Denomination: 5,
+					CoinCount:    2,
+				},
+				{
+					ID:           2,
+					Denomination: 10,
+					CoinCount:    2,
+				},
+			},
+			draws: []int{7, 6},
+			want:  []int{3, 2, 0},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			i := 0
+			draw := func() int {
+				r := tt.draws[i]
+				i++
+				return r
+			}
+
+			actual := shakePot(tt.pot, len(tt.draws), draw)
+			assert.Equal(t, tt.want, actual)
+		})
+	}
 }
